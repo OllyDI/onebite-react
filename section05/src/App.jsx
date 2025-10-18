9a8b587fb07e8e843fc20cce038b3e49ca2fe8d4
OllyDI <choiseungho11@hanmail.net>
2025-10-18 23:24:35 +0900

    25.10.18

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,17 +1,29 @@
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
-import './App.css'
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import Button from './components/Button';
+import './App.css';
 /*
   리액트의 컴포넌트는 첫 글자가 무조건 대문자로 작성해야 인식됨
 */
 
 function App() {
+
+  const buttonProps = {
+    text: "메일",
+    color: "red",
+    a: 1,
+    b: 2,
+    c: 3,
+  }
+
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <Button {...buttonProps}/>
+      <Button text={"카페"}/>
+      <Button text={"블로그"}>
+        <Header />
+      </Button>
     </>
   )
 }
