f8bab4a70d7bb6b952663ef31dc860c1cd4f80ed
OllyDI <choiseungho11@hanmail.net>
2025-11-02 21:06:30 +0900

    25.11.02

--- section12/src/pages/Edit.jsx
+++ section12/src/pages/Edit.jsx
@@ -1,11 +1,18 @@
-import { useParams } from 'react-router-dom'
+import Header from "../components/Header"
+import Button from "../components/Button"
+
 
 const Edit = () => {
 
-    const params = useParams();
 
     return (
-        <div>{params.id + 1}번</div>
+        <div>
+            <Header 
+                title={'일기 수정하기'}
+                leftChild={<Button text={'뒤로가기'}/>}
+                rightChild={<Button text={'삭제하기'} type={'NEGATIVE'}/>}
+            />
+        </div>
     )
 }
 
