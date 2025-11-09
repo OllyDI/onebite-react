cba65c537482b824acefb73d458673c137312695
OllyDI <choiseungho11@hanmail.net>
2025-11-09 17:24:39 +0900

    25.11.09

--- ReviewPractice/react/src/components/userRegister.jsx
+++ ReviewPractice/react/src/components/userRegister.jsx
@@ -2,9 +2,17 @@ import './userRegister.css'
 
 const UserRegister = () => {
     return (
-        <div>
-            register
-        </div>
+        <form className="UserLogin" >
+            <div className="user_info">
+                <input id='id' placeholder="아이디"/>
+                <input type="password" id='pw' placeholder="패스워드"/>
+                <input type="name" id='name' placeholder="이름"/>
+                <Button text={'회원가입'} type={'POSITIVE'}/>
+            </div>
+            <div className="user_reg">
+                <p>로그인</p>
+            </div>
+        </form>
     )
 }
 
